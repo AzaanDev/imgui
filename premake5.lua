21d30f967c404324b0e0929cc9caefebbaf77e59
Azaan <xxx.azaan.ali@gmail.com>
2022-04-22 01:36:43 -0400

    update premake and gitignore

--- premake5.lua
+++ premake5.lua
@@ -17,7 +17,11 @@ project "imgui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+		"./backends/imgui_impl_sdl.h",
+		"./backends/imgui_impl_sdl.cpp",
+		"./backends/imgui_impl_opengl3.h",
+		"./backends/imgui_impl_opengl3.cpp"
 	}
 
 filter "system:windows"
