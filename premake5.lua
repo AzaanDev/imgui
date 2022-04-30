20cc4ef40a13f0bd06e375e184754107721d1d90
Azaan <xxx.azaan.ali@gmail.com>
2022-04-30 01:29:18 -0400

    premake fix

--- premake5.lua
+++ premake5.lua
@@ -1,37 +1,32 @@
 project "imgui"
-  kind "StaticLib"
-  language "C++"
-  cppdialect "C++17"
-
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+	
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir    ("bin-obj/" .. outputdir .. "/%{prj.name}")
-
+	
+	includedirs      {"../imgui", "%{IncludeDir.SDL2}/include",}
+	
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp",
-		"./backends/imgui_impl_sdl.h",
-		"./backends/imgui_impl_sdl.cpp",
-		"./backends/imgui_impl_opengl3.h",
-		"./backends/imgui_impl_opengl3.cpp"
+		"*.h",
+		"*.cpp",
+		"backends/imgui_impl_sdl.h",
+		"backends/imgui_impl_sdl.cpp",
+		"backends/imgui_impl_opengl3.h",
+		"backends/imgui_impl_opengl3.cpp"
 	}
 
-filter "system:windows"
-	systemversion "latest"
-	staticruntime "On"
+	filter "system:windows"
+		systemversion "latest"
 
-filter "configurations:Debug"
-  runtime "Debug"
-  symbols "on"
+  	filter "configurations:Debug"
+      	runtime "Debug"
+      	symbols "On"
 
-filter "configurations:Release"
-  runtime "Release"
-  optimize "on"
+   	filter "configurations:Release"
+      	runtime "Release"
+      	optimize "On"
+		
