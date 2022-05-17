fe4fa78cf3ac2767353edbd0097990e903585d75
Azaan <47795154+AzaanDev@users.noreply.github.com>
2022-05-17 00:32:56 -0400

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,22 +1,25 @@
 project "imgui"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++17"
+    cppdialect "C++17"
 	staticruntime "off"
 	
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir    ("bin-obj/" .. outputdir .. "/%{prj.name}")
-	
-	includedirs      {"../imgui", "%{IncludeDir.SDL2}/include",}
-	
+
 	files
 	{
-		"*.h",
-		"*.cpp",
-		"backends/imgui_impl_sdl.h",
-		"backends/imgui_impl_sdl.cpp",
-		"backends/imgui_impl_opengl3.h",
-		"backends/imgui_impl_opengl3.cpp"
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
 	}
 
 	filter "system:windows"
@@ -29,4 +32,5 @@ project "imgui"
    	filter "configurations:Release"
       	runtime "Release"
       	optimize "On"
+        symbols "off"
 		
